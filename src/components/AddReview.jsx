3a4695926ee934907b5ae1e404c594875f16847c
orwel009 <orwelvincent1@gmail.com>
2024-06-13 01:10:50 +0530

    AddReview  commit

---
+++ src/components/AddReview.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import Navbar from './Navbar'
+
+const AddReview = () => {
+    const [data,getData] = useState(
+        {
+            "name":"",
+            "year":"",
+            "duration":"",
+            "rating":""
+        }
+    )
+    const inputHandler=(event)=>{
+        getData({...data,[event.target.name]:event.target.value})
+    }
+    const readValue = ()=>{
+        console.log(data)
+    }
+  return (
+    <div>
+        <Navbar/>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">MovieName</label>
+                            <input type="text" className="form-control" name='name' value={data.name} onChange={inputHandler} />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Year</label>
+                            <input type="text" className="form-control" name='year' value={data.year}  onChange={inputHandler} />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Duration</label>
+                            <input type="text" className="form-control" name='duration' value={data.duration} onChange={inputHandler} />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Rating</label>
+                            <input type="text" className="form-control" name='rating' value={data.rating} onChange={inputHandler} />
+                        </div>
+                        
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <button className="btn btn-success" onClick={readValue}>AddReview</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AddReview
